09dded2d60fa3f45da08ff245c109c8eeca2828b
Shiva Saxena <shivasaxena911@gmail.com>
2022-08-13 19:07:19 +0530

    fix tests

--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -9,11 +9,11 @@ class NotificationTest(APITestCase):
         url = reverse("subscriptions-list")
         data = {
             "push_service_url": "https://some.pushservice.com/test",
-            "client_private_key": "BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI=",
-            "client_public_key": "FPssNDTKnInHVndSTdbKFw==",
+            "subscription_public_key": "BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI=",
+            "subscription_auth": "FPssNDTKnInHVndSTdbKFw==",
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         expected_keys = list(data.keys())
         expected_keys.extend(["id", "created_at"])
-        self.assertEqual(set(expected_keys).issubset(set(response.json().keys())))
+        self.assertTrue(set(expected_keys).issubset(set(response.json().keys())))
