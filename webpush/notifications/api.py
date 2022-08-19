7184fb2515e54789265d5e135fe737effc457bfe
Shiva Saxena <shivasaxena911@gmail.com>
2022-08-19 17:16:36 +0530

    feat(notification): trigger push notifications through API call (#6)

--- webpush/notifications/api.py
+++ webpush/notifications/api.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework import status
 
 from .serializers import NotificationSerializer
+from .tasks import spawn_webpush_requests_task
+from .services import prepare_notification_data
 
 
 class NotificationViewSet(viewsets.ViewSet):
@@ -10,5 +11,7 @@ class NotificationViewSet(viewsets.ViewSet):
     def create(self, request):
         serializer = NotificationSerializer(data=request.data)
         if serializer.is_valid():
+            notification_data = prepare_notification_data(serializer.validated_data)
+            spawn_webpush_requests_task.delay(notification_data=notification_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
